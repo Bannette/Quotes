3e8bb8a33de930c4d681a6836c1671f966494001
Bannette <banetaloo3@gmail.com>
2019-07-07 20:29:34 +0300

    modified the app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: Quote []= [
-    {id:1, name:'Dont let what you can not interfer with interfer with you'},
-    {id:2, name:'Succesful and unsuccesful dont vary in ability they vary in their desires to do things'},
-    {id:3, name:'Strive for progress not perfection'},
-    {id:4, name:'Theres no shortcuts to any place worth going'}
-  ];
 }
