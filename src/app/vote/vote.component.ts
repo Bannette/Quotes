acd8e6033d1cdf183ce3fa8da026b2e84b262c06
Bannette <banetaloo3@gmail.com>
2019-07-08 12:28:37 +0300

    added upvote and downvote

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Quote} from '../quote'
 
+
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
@@ -10,6 +11,7 @@ export class VoteComponent implements OnInit {
   @Output() isComplete = new EventEmitter<boolean>();
   uvotes = 0;
   dvotes = 0;
+  quotes: any;
   upVote(){
     this.uvotes = this.uvotes + 1;
   }
@@ -21,6 +23,38 @@ export class VoteComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getHighest();
+  }
+
+  addVote({ vote, index }: { vote; index; }) {
+    if (vote) {
+      this.quotes[index].upvote += 1;
+      this.getHighest();
+    } else {
+      this.quotes[index].downvote += 1;
+    }
+  }
+
+  getHighest() {
+    let highest = 0;
+    let highestQuote: Quote;
+    for (const quote of this.quotes) {
+      if (quote.upvote > highest) {
+        highest = quote.upvote;
+        highestQuote = quote;
+      }
+    }
+    this.changeHighest(highestQuote);
+  }
+
+  changeHighest(highest: Quote) {
+    for (const quote of this.quotes) {
+      if (quote.id === highest.id) {
+        quote.isHighest = true;
+      } else {
+        quote.isHighest = false;
+      }
+    }
   }
 
 }
